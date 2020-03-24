da6fa8fabdf70e43b5d07a00ef99fc612a4bd29a
Truong <truongphan97gl@gmail.com>
2020-03-24 16:49:11 -0400

    Updates

---
+++ precache-manifest.c0ddf9a6c8ad9a0618bbd38ea541b68a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d85f9a9307b9f55de2a1b6d91f0a7d",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "3368dc13faaaec1c808c",
+    "url": "/monster-app/static/css/main.f80aec2c.chunk.css"
+  },
+  {
+    "revision": "52ab36c179d2e860984b",
+    "url": "/monster-app/static/js/2.1662debb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.1662debb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3368dc13faaaec1c808c",
+    "url": "/monster-app/static/js/main.ac1162ce.chunk.js"
+  },
+  {
+    "revision": "0c571607ebdb9a69c537",
+    "url": "/monster-app/static/js/runtime-main.0e395675.js"
+  }
+]);
